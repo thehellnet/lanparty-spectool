1c92b85998d53fd23b1b0b0fa8d9bc37978936b8
sardylan <sardylan@gmail.com>
2020-08-16 14:08:59 +0200

    Adds Tests

---
+++ LanPartySpecToolTests/protocol/CommandParserTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace LanPartySpecTool.protocol
+{
+    public class CommandParserTest
+    {
+        [Test]
+        public void TestParseNull()
+        {
+            const string input = null;
+            var actual = CommandParser.Parse(input);
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void TestParseEmpty()
+        {
+            const string input = "";
+            var actual = CommandParser.Parse(input);
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void TestParseNotValid()
+        {
+            const string input = "test";
+            var actual = CommandParser.Parse(input);
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void TestParseEmptyJson()
+        {
+            const string input = "{}";
+            var actual = CommandParser.Parse(input);
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void TestParseValidJsonInvalidAction()
+        {
+            const string input = "{\"action\": \"test\"}";
+            var actual = CommandParser.Parse(input);
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void TestParseValid()
+        {
+            const string input = "{\"action\": \"JOIN_SPECTATE\"}";
+            var expected = new Command(CommandAction.JoinSpectate);
+            var actual = CommandParser.Parse(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
