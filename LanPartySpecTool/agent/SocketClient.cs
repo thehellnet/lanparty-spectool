48868a3994fab663f90d81251cdd0dbe5d078661
sardylan <sardylan@gmail.com>
2020-08-16 14:11:03 +0200

    Completes integration in socket client

--- LanPartySpecTool/agent/SocketClient.cs
+++ LanPartySpecTool/agent/SocketClient.cs
@@ -3,7 +3,6 @@ using System.Net.Sockets;
 using LanPartySpecTool.protocol;
 using LanPartySpecTool.utility;
 using log4net;
-using Newtonsoft.Json.Linq;
 
 namespace LanPartySpecTool.agent
 {
@@ -11,7 +10,10 @@ namespace LanPartySpecTool.agent
     {
         public delegate void NewCommandHandler(ulong clientId, Command command);
 
+        public delegate void ClientStopHandler(ulong clientId);
+
         public event NewCommandHandler OnNewCommand;
+        public event ClientStopHandler OnClientStop;
 
         private static readonly ILog Logger = LogManager.GetLogger(typeof(SocketClient));
 
@@ -22,6 +24,10 @@ namespace LanPartySpecTool.agent
         {
             _id = id;
             _socket = socket;
+
+            _socket.ReceiveTimeout = 0;
+
+            OnStop += () => OnClientStop?.Invoke(_id);
         }
 
         public override void Start()
@@ -43,8 +49,20 @@ namespace LanPartySpecTool.agent
         protected override void Job()
         {
             var rawData = ReadDataFromSocket();
-            var command = ParseCommand(rawData);
-            InvokeNewCommandEvent(command);
+            if (rawData == null)
+            {
+                return;
+            }
+
+            var command = CommandParser.Parse(rawData);
+            if (command == null)
+            {
+                Logger.Warn($"Invalid command: {rawData}");
+                return;
+            }
+
+            Logger.Debug($"Invoking new command event for client {_id}");
+            OnNewCommand?.Invoke(_id, command);
         }
 
         private string ReadDataFromSocket()
@@ -52,38 +70,23 @@ namespace LanPartySpecTool.agent
             Logger.Debug($"Reading data from socket {_id}");
 
             var buffer = new byte[8192];
-            var bytesRead = _socket.Receive(buffer);
-            var rawData = BitConverter.ToString(buffer, 0, bytesRead);
-            return rawData;
-        }
 
-        private static Command ParseCommand(string rawData)
-        {
-            Logger.Debug($"Parsing command");
+            int bytesRead;
 
-            var rawCommand = JObject.Parse(rawData);
-
-            var actionString = rawCommand.GetValue("action")?.ToString();
-            if (actionString == null)
+            try
             {
+                bytesRead = _socket.Receive(buffer);
+            }
+            catch (SocketException)
+            {
+                StopJob();
                 return null;
             }
 
-            var action = (CommandAction) Enum.Parse(typeof(CommandAction), actionString);
-
-            var command = new Command(action);
-            return command;
-        }
-
-        private void InvokeNewCommandEvent(Command command)
-        {
-            if (command == null)
-            {
-                return;
-            }
-
-            Logger.Debug($"Invoking new command event for client {_id}");
-            OnNewCommand?.Invoke(_id, command);
+            var rawData = new char[bytesRead];
+            Array.Copy(buffer, 0, rawData, 0, bytesRead);
+            var data = new string(rawData);
+            return data;
         }
     }
 }
